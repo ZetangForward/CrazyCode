2697c7c4a2d8236f5901d47833db4afadcbc04ef
v-zetang <v-zetang@microsoft.com>
2023-10-31 19:50:13 +0800

    1

--- tutils.py
+++ tutils.py
@@ -3,10 +3,12 @@ import os
 from termcolor import colored  
 from typing import List, Dict
 
+
 print(colored('Load CrazyCode -- Road is under your feet, ZetangForward', 'red'))  
 
-def print_c(s, color='green'):
-    print(colored(s, color=color))
+
+def print_c(s, c='green'):
+    print(colored(s, color=c))
 
 
 def load_jsonl(file_path, return_format="list"):
@@ -17,6 +19,7 @@ def load_jsonl(file_path, return_format="list"):
     else:
         pass
 
+
 def sample_dict_items(dict_, n=3):
     print_c(f"sample {n} items from dict", 'green')
     cnt = 0
@@ -26,10 +29,23 @@ def sample_dict_items(dict_, n=3):
         if cnt == n:
             break
 
-def filter_jsonl_lst(lst: List[Dict], kws=None):
+
+def filter_jsonl_lst(lst: List[Dict], kws: List[str]=None):
+    """
+    
+    """
     if kws is None:
         res = lst
         print_c("Warning: no filtering, return directly!")
     else:
         res = [dict([(k, item.get(k)) for k in kws]) for item in lst]
+    return res
+
+
+def build_dict_from_jsonl_lst(lst: List[Dict], kws: str=None):
+    """
+    
+    """
+    assert kws is not None, print_c("kws must not None!", "blue")
+    res = dict([(kws, item[kws]) for item in lst])
     return res
