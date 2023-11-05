7da4c9ddb8aab34c5c2b6795c311be962c5ea552
v-zetang <v-zetang@microsoft.com>
2023-11-05 22:31:28 +0800

    dsa

--- auto_grab_gpu.sh
+++ auto_grab_gpu.sh
@@ -4,12 +4,12 @@ while true; do
     # 使用nvidia-smi命令检查GPU状态  
     if nvidia-smi | grep -q 'No running processes found'; then  
         cd /zecheng/GrabGPU/  
-        ./gg 30 30 0,1,2,3,4,5,6,7  
-        sleep 10  
+        ./gg 30 240 0,1,2,3,4,5,6,7  
+        break  
     else  
         # 如果GPU不空闲，打印当前时间并休眠5分钟  
         gpustat
         date  
-        sleep 300  
+        sleep 5  
     fi  
 done  
