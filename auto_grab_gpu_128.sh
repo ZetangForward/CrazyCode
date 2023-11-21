afdfbf4c0f6f94f7e0bdcc3436b60c102ad0c41c
ZetangForward <zctang2000@gmail.com>
2023-11-21 16:10:41 +0000

    1

--- auto_grab_gpu_128.sh
+++ auto_grab_gpu_128.sh
@@ -7,7 +7,7 @@ while true; do
         ./gg 30 240 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15  
         break  
     else  
-        # 如果GPU不空闲，打印当前时间并休眠3分钟  
+        # 如果GPU不空闲，打印当前时间并休眠1分钟  
         gpustat
         date  
         sleep 60
