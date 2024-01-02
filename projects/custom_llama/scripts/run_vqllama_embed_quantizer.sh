c96819226618a8f6144a6ec9b030d943d1d3ffb3
ZetangForward <1>
2024-01-03 01:09:48 +0800

    dsa

--- projects/custom_llama/scripts/run_vqllama_embed_quantizer.sh
+++ projects/custom_llama/scripts/run_vqllama_embed_quantizer.sh
@@ -2,7 +2,7 @@ torchrun \
     --nnodes=2 \
     --nproc-per-node=4 \
     --max-restarts=3 \
-    --rdzv_id=543 \
+    --rdzv_id=111 \
     --rdzv_backend=c10d \
-    --rdzv_endpoint=10.184.185.106:4321 \
+    --rdzv_endpoint=10.184.185.106:4331 \
     train_pl_v2.py;
