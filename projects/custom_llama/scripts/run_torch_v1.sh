33b7eaa696550be345f70ac66d83c32a5106be62
ZetangForward <1>
2024-01-03 13:23:08 +0800

    dsa

--- projects/custom_llama/scripts/run_torch_v1.sh
+++ projects/custom_llama/scripts/run_torch_v1.sh
@@ -1,6 +1,9 @@
 x=($(python -c "import torch;import os;x = ['106','117','154','146','150','122','124','145','126','138','139','137','151','105','136','142'];l = ','.join([f'GCRHYPC{e}' for e in x]).split(',');print(l[0], len(l), torch.cuda.device_count(), l.index(os.environ['HOSTNAME']))"))
 
 export NCCL_DEBUG=INFO 
+export NCCL_IB_DISABLE=1
+export NCCL_IBEXT_DISABLE=1 
+
 
 echo "NCCL_DEBUG: ${NCCL_DEBUG}"
 echo ${x[0]} ${x[1]} ${x[2]} ${x[3]}
