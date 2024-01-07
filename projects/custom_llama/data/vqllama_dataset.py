0a94f8ba55f147290a1890000514912480ff8b1e
ZetangForward <1>
2024-01-07 16:29:11 +0000

    dsadsa

--- projects/custom_llama/data/vqllama_dataset.py
+++ projects/custom_llama/data/vqllama_dataset.py
@@ -6,29 +6,94 @@ import random
 import torch  
 import torch.nn as nn 
 from transformers import PreTrainedTokenizer, LlamaConfig, LlamaForCausalLM  
-from torch.utils.data import Dataset
+from torch.utils.data import DataLoader, Dataset 
 from modelzipper.tutils import *
 
 
-class VQLLaMADataset(Dataset):
-    
-    def __init__(self, args, codebook_file, tokenizer: PreTrainedTokenizer, numerical_token):  
-        self.tokenizer = tokenizer  
-        self.max_seq_len = args.model_max_length
-        self.numerical_token = numerical_token
-        self.numerical_token_id = self.tokenizer.convert_tokens_to_ids(self.numerical_token)
-        
-        ## Load SVG data
-        with open(svg_file, "r") as f2:
-            self.content = [json.loads(line) for line in f2]
-        
-        ## whether to process numerical values in svg paths
-        self.numerical_mode = True
-        if args.numerical_mode is not None:
-            self.numerical_mode = args.numerical_mode
-        
+class BasicDataset(Dataset):
+
+    PROMPT_TEMPLATE = "Keywords: {keywords} #begin:"
+
+    def __init__(self, content, tokenizer, mode="train", svg_token=None, max_text_length=64, max_svg_length=1024) -> None:
+        super().__init__()
+
+        self.tokenizer = tokenizer
+        self.content = content
+        self.mode = mode
+        self.svg_token = svg_token
+        self.max_text_length = max_text_length
+        self.max_svg_length = max_svg_length
+
     def __len__(self):
         return len(self.content)
+
+    def __getitem__(self, index):
+        svg_quantised = self.content[index]["xs_quantised"]
+        keywords = self.content[index]["keywords"]
+        prompts = self.PROMPT_TEMPLATE.format(keywords=keywords)
+        
+        if self.svg_token is not None:
+            prompts = prompts + " " + self.svg_token
+
+        seq_inputs = self.tokenizer(
+            prompts, 
+            padding="max_length", 
+            truncation=True, 
+            max_length=self.max_text_length,
+            return_tensors="pt",
+        )
+        text_input_ids = seq_inputs.input_ids[0]
+        text_attention_mask = seq_inputs.attention_mask[0]
+        text_labels = torch.where(
+            text_input_ids != self.tokenizer.pad_token_id, text_input_ids, -100
+        )
+
+        if self.svg_token is not None:  # utilize svg_token as the end of the text
+            text_input_ids = text_input_ids[:-1]
+            text_attention_mask = text_attention_mask[:-1]
+            text_labels = text_labels[:-1]
+
+        return {
+            "input_prompt_ids": text_input_ids,
+            "attention_mask": text_attention_mask,
+            "labels": text_labels,
+            "svg_quantised": svg_quantised,
+        }
+
+
+class VQDataCollator:
+    """
+    a variant of callate_fn that pads according to the longest sequence in
+    a batch of sequences
+    """
+    def __init__(self):
+        ...
+
+    
+
+class VQLLaMAData:
+    def __init__(self, args, vq_svg_file, tokenizer: PreTrainedTokenizer, split="train"):  
+
+        self.tokenizer = tokenizer  
+        self.split = split
+        self.max_seq_len = args.model_max_length
+        content = auto_read_data(vq_svg_file) ## Load VQSVG data
+        self.valid_data = content[:2000]
+        self.train_data = content[2000:]
+
+
+    def train_dataloader(self) -> DataLoader:
+        pass
+
+
+    def valid_dataloader(self) -> DataLoader:
+        pass
+
+
+    def predict_dataloader(self) -> DataLoader:
+        pass
+
+    
     
     def extract_numerical(self, path_data, numerical_token):  
         ## match all numericals 
--- projects/custom_llama/scripts/inference_node0.sh
+++ projects/custom_llama/scripts/inference_node0.sh
@@ -2,7 +2,7 @@ LOG_OUTPUT="/workspace/zecheng/modelzipper/projects/custom_llama/Logs"
 
 mkdir -p ${LOG_OUTPUT}
 
-for i in {0..1}; do  
+for i in {4..6}; do  
     device=$((i - 0))
     CUDA_VISIBLE_DEVICES=${device} python switch_codebook.py SNAP_ID=${i}  > ${LOG_OUTPUT}/inference_${i}.log 2>&1 &  
 done 
