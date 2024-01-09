8a84d91366be1b21e3c08c1d0d219bd311fc200e
ZetangForward <1>
2024-01-09 19:18:34 +0800

    dsa

--- projects/custom_llama/scripts/test.sh
+++ projects/custom_llama/scripts/test.sh
@@ -2,7 +2,7 @@ deepspeed --num_gpus 1 \
     --num_nodes 1 \
     train_vqllama.py \
     --model_name_or_path "/zecheng2/model_hub/Llama-2-7b-hf" \
-    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_70/inference_full_data_compress_1_snaps_4.pkl" \
+    --data_path "/zecheng2/svg/icon-shop/pkl_data/full_data.pkl" \
     --output_dir "/zecheng2/vqllama/vqllama_llama/version_0" \
     --num_train_epochs 40 \
     --model_max_length 1500 \
