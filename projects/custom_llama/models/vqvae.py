4c7cb1bba9b2861d421269e83d5b325ccf1bfcf6
ZetangForward <1>
2024-01-10 14:52:32 +0800

    dsadsa

--- projects/custom_llama/models/vqvae.py
+++ projects/custom_llama/models/vqvae.py
@@ -25,11 +25,86 @@ def average_metrics(_metrics):
     return {key: sum(vals)/len(vals) for key, vals in metrics.items()}
 
 
-def normalize_func(tensor, min_val=0, max_val=200):
-    # normalize to [-1, 1]
-    normalized_tensor = (tensor - min_val) / (max_val - min_val)
-    normalized_tensor = normalized_tensor * 2 - 1
-    return normalized_tensor
+def remove_padding(x, padding_mask):
+    """
+    x: (batch_size x) seq_len num_bins
+    padding: seq_len x num_bins
+
+    if batch, return List[Tensor]
+    if tensor, return Tensor
+    """
+    if x.ndim == 2:  # seq_len x num_bins
+        return x[:padding_mask.sum(), :]
+    elif x.ndim == 3:  # batch_size x seq_len x num_bins
+        res = []
+        for i in range(x.size(0)):
+            res.append(x[i, :padding_mask[i].sum(), :])
+        return res
+
+
+def postprocess(x, padding_mask=None, path_interpolation=True):
+    """
+    postprocess the generated results
+
+    x: batch_size x seq_len x 9
+    padding_mask: batch_size x seq_len
+    path_interpolation: whether to interpolate the path
+    """
+    dtype = x.dtype
+    if x.ndim == 2:
+        x = x.unsqueeze(0)
+    if padding_mask.ndim == 1:
+        padding_mask = padding_mask.unsqueeze(0)
+
+    if path_interpolation:  
+        # conduct path interpolation
+        # return List[Tensor]
+        batch_size = x.size(0)
+        x = remove_padding(x, padding_mask)  # remove the padding
+        full_x = []
+        for i in range(batch_size):
+            current_path = []
+            last_x3, last_y3 = None, None
+            seq_len = x[i].size(0)
+            for j in range(seq_len):
+                row = x[i][j]
+                cmd = 100 * t.round(row[0] / 100).item()
+                cmd = 1 if cmd == 100 else 2 if cmd == 200 else 0
+                x0, y0, x1, y1, x2, y2, x3, y3 = map(lambda coord: min(max(coord, 0), 200), row[1:].tolist())
+                if last_x3 is not None and (last_x3 != x0 or last_y3 != y0):
+                    # if the current row's start point is not the same as the previous row's end point
+                    current_path.append([0, last_x3, last_y3, 0, 0, 0, 0, x0, y0])
+                if cmd in [0, 100]:
+                    # if the current row is M or L, set control point to 0
+                    x1, y1, x2, y2 = 0, 0, 0, 0
+                current_path.append([cmd, x0, y0, x1, y1, x2, y2, x3, y3])
+                last_x3, last_y3 = x3, y3  # update the last end point
+            full_x.append(t.tensor(current_path, dtype=dtype))
+    
+    else:  # no path interpolation
+        if x.size(-1) == 9:
+            # first remove the 1, 2 columns
+            m_x = t.cat((x[:, :, :1], x[:, :, 3:]), dim=2)
+        else:
+            m_x = x
+        # find the right command value
+        m_x[:, :, 0] = t.round(m_x[:, :, 0] / 100) * 100
+        # clip all the value to max bins 
+        m_x = t.clamp(m_x, 0, 200)
+        # process the M and L path                                                              
+        m_x[:, :, 1:5][m_x[:, :, 0] != 200] = 0
+        # add to extra column to satisfy the 9 columns
+        x_0_y_0 = t.zeros((m_x.size(0), m_x.size(1), 2), dtype=m_x.dtype, device=m_x.device)
+        x_0_y_0[:, 1:, 0] = m_x[:, :-1, -2]  # x_3 of the previous row
+        x_0_y_0[:, 1:, 1] = m_x[:, :-1, -1]  # y_3 of the previous row
+        full_x = t.cat((m_x[:, :, :1], x_0_y_0, m_x[:, :, 1:]), dim=2)
+        # replace the command value to 0, 1, 2
+        full_x[:, :, 0][full_x[:, :, 0] == 100] = 1
+        full_x[:, :, 0][full_x[:, :, 0] == 200] = 2
+        # remove the padding
+        full_x = remove_padding(full_x, padding_mask)
+
+    return full_x
 
 
 def _loss_fn(loss_fn, x_target, x_pred, cfg, padding_mask=None):
@@ -81,6 +156,20 @@ class VQVAE(nn.Module):
         self.encoders = nn.ModuleList()
         self.decoders = nn.ModuleList()
 
+        def normalize_func(self, tensor, min_val=0, max_val=200):
+            # normalize to [-1, 1]
+            normalized_tensor = (tensor - min_val) / (max_val - min_val)
+            normalized_tensor = normalized_tensor * 2 - 1
+            return normalized_tensor
+
+
+        def denormalize_func(self, normalized_tensor, min_val=0, max_val=200):
+            tensor = (normalized_tensor + 1) / 2
+            tensor = tensor * (max_val - min_val) + min_val
+            tensor = t.round(tensor).long()
+            return tensor
+        
+        
         def _block_kwargs(level):
             this_block_kwargs = dict(block_kwargs)
             this_block_kwargs["width"] *= self.multipliers[level]
@@ -140,31 +229,26 @@ class VQVAE(nn.Module):
         x_out = self.postprocess(x_out)
         return x_out
 
-    def decode(self, zs, start_level=0, end_level=None, bs_chunks=1):
-        z_chunks = [t.chunk(z, bs_chunks, dim=0) for z in zs]
-        x_outs = []
-        for i in range(bs_chunks):
-            zs_i = [z_chunk[i] for z_chunk in z_chunks]
-            x_out = self._decode(
-                zs_i, start_level=start_level, end_level=end_level)
-            x_outs.append(x_out)
-        return t.cat(x_outs, dim=0)
-
-    def _encode(self, x, start_level=0, end_level=None):
-        # Encode
+    def decode(self, zs, start_level=0, end_level=None, padding_mask=None, path_interpolation=True, return_postprocess=True):
         if end_level is None:
             end_level = self.levels
-        x_in = self.preprocess(x)
-        xs = []
-        for level in range(self.levels):
-            encoder = self.encoders[level]
-            x_out = encoder(x_in)
-            xs.append(x_out[-1])
-        zs = self.bottleneck.encode(xs)
-        return zs[start_level:end_level]
+            
+        xs_quantised = self.bottleneck.decode(
+            zs, start_level=start_level, end_level=end_level
+        )
+        
+        # Use only lowest level
+        decoder, x_quantised = self.decoders[start_level], xs_quantised[0:1]
+        x_out = decoder(x_quantised, all_levels=False)
+        x_out = self.denormalize_func(x_out)
+
+        if return_postprocess:
+            x_out = postprocess(x_out, padding_mask, path_interpolation)  
+        return x_out
+
 
     def encode(self, x, start_level=0, end_level=None):
-        x = normalize_func(x) # normalize to [-1, 1]
+        x = self.normalize_func(x) # normalize to [-1, 1]
         x_in = x.permute(0, 2, 1)  # x_in (32, 9, 256)
         xs = []
 
@@ -172,14 +256,11 @@ class VQVAE(nn.Module):
             end_level = self.levels
 
         for level in range(self.levels):
-            encoder = self.encoders[level]
-            x_out = encoder(x_in)
+            x_out = self.encoders[level](x_in)
             xs.append(x_out[-1])
-
+            
         zs, xs_quantised = self.bottleneck(xs[start_level:end_level], just_return_zs=True)
-
         xs_quantised = [tmp.permute(0, 2, 1) for tmp in xs_quantised]
-
         return zs, xs_quantised
 
 
@@ -196,7 +277,7 @@ class VQVAE(nn.Module):
         """
         metrics = {}
         
-        x = normalize_func(x) # normalize to [-1, 1]
+        x = self.normalize_func(x) # normalize to [-1, 1]
         x_in = x.permute(0, 2, 1).float()  # x_in (32, 9, 256)
         xs = []
 
