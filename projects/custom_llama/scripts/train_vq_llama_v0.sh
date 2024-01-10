fee4246c60a60802348207d05d625424c55780a2
ZetangForward <zecheng.tang@foxmail.com>
2024-01-10 21:56:31 +0800

    dsa

---
+++ projects/custom_llama/scripts/train_vq_llama_v0.sh
@@ -0,0 +1,31 @@
+deepspeed --num_gpus 16 \
+    --num_nodes 4 \
+    --master_addr worker-0 \
+    --master_port 7329 \
+    --hostfile configs/machine/hostfile_v64_sxm4 \
+    train_vqllama.py \
+    --model_name_or_path "/zecheng2/model_hub/Llama-2-7b-hf" \
+    --data_path "/zecheng2/svg/icon-shop/pkl_data/full_data.pkl" \
+    --output_dir "/zecheng2/vqllama/vqllama_llama/version_0" \
+    --num_train_epochs 60 \
+    --model_max_length 1500 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 8 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 80 \
+    --save_total_limit 10 \
+    --learning_rate 3e-6 \
+    --warmup_steps 20 \
+    --logging_steps 1 \
+    --dataloader_num_workers 16 \
+    --lr_scheduler_type "cosine" \
+    --report_to "tensorboard" \
+    --gradient_checkpointing True \
+    --deepspeed configs/deepspeed/stage3.json \
+    --fp16 True \
+    --remove_unused_columns False \
+    --config_path "configs/deepspeed/vqvae_config.yaml";
+
+
