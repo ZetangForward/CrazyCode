f91816f5257e69e32c3a1dc859a0b09dfcfceae9
ZetangForward <zecheng.tang@foxmail.com>
2024-01-11 12:52:12 +0800

    dsa

--- projects/custom_llama/scripts/test.sh
+++ projects/custom_llama/scripts/test.sh
@@ -2,22 +2,21 @@ deepspeed --num_gpus 8 \
     --num_nodes 1 \
     train_vqllama.py \
     --model_name_or_path "/zecheng2/model_hub/Llama-2-7b-hf" \
-    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_merged.pkl" \
+    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_7.pkl" \
     --output_dir "/zecheng2/vqllama/vqllama_llama/version_0" \
     --num_train_epochs 60 \
     --model_max_length 1500 \
-    --per_device_train_batch_size 36 \
-    --per_device_eval_batch_size 36 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "epoch" \
     --save_strategy "steps" \
-    --max_steps 100000 \
     --save_steps 80 \
     --save_total_limit 10 \
     --learning_rate 3e-6 \
     --warmup_steps 20 \
     --logging_steps 1 \
-    --dataloader_num_workers 14 \
+    --dataloader_num_workers 12 \
     --lr_scheduler_type "cosine" \
     --report_to "tensorboard" \
     --gradient_checkpointing True \
