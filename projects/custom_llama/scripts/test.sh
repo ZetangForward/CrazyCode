b2f722e40242ae3a447ab46f459a3af5a7546433
ZetangForward <zecheng.tang@foxmail.com>
2024-01-11 12:12:01 +0800

    dsa

--- projects/custom_llama/scripts/test.sh
+++ projects/custom_llama/scripts/test.sh
@@ -2,7 +2,7 @@ deepspeed --num_gpus 8 \
     --num_nodes 1 \
     train_vqllama.py \
     --model_name_or_path "/zecheng2/model_hub/Llama-2-7b-hf" \
-    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_7.pkl" \
+    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_merged.pkl" \
     --output_dir "/zecheng2/vqllama/vqllama_llama/version_0" \
     --num_train_epochs 60 \
     --model_max_length 1500 \
