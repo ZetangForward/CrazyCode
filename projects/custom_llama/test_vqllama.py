393fe9717f8e92c2ced932916d8685be22014982
ZetangForward <zecheng.tang@foxmail.com>
2024-01-11 21:17:17 +0800

    dsa

--- projects/custom_llama/test_vqllama.py
+++ projects/custom_llama/test_vqllama.py
@@ -9,7 +9,7 @@ from models.vqllama import VQSVGLlama
 from data.vqllama_dataset import VQLLaMAData
 from models.vqvae import VQVAE
 from train_vqllama import smart_tokenizer_and_embedding_resize
-from utils.visualize_svg import sanint_check_svg_tensor, convert_svg
+from utils.visualize_svg import sanint_check_svg_tensor, convert_svg, merge_images
 
 
 IGNORE_INDEX = -100
@@ -24,7 +24,8 @@ DEFAULT_SVG_BEGIN_TOKEN = "<SVG>"
 class TestConfig:
     vqvae_config_path: str = field(default=None)
     tokenier_config_path: str = field(default=None)
-    model_name_or_path: str = field(default=None)
+    version: int = field(default=None)
+    epoch: int = field(default=None)
     data_path: str = field(default=None)
     predict_batch_size: int = field(default=1)
     dataloader_num_workers: int = field(default=0)
@@ -52,6 +53,10 @@ def predict_loop(model, vqvae, dataloader, tokenizer, max_generate_length=1024, 
             text_attention_mask = batch_.get("text_attention_mask")
             golden_svg_path = batch_.get("svg_path")
             
+            text_input_ids = text_input_ids.to(model.device) if text_input_ids is not None else None
+            text_attention_mask = text_attention_mask.to(model.device) if text_attention_mask is not None else None
+            golden_svg_path = golden_svg_path.to(model.device) if golden_svg_path is not None else None
+            
             with torch.no_grad():
                 _, post_processed_ids = model.generate(  # List[Tensor]
                     text_input_ids=text_input_ids,
@@ -66,12 +71,12 @@ def predict_loop(model, vqvae, dataloader, tokenizer, max_generate_length=1024, 
                     
                     text = tokenizer.decode(text_input_ids[i], skip_special_tokens=True)
                     
-                    cur_batch_res.append(
+                    cur_batch_res.append(  # move to the CPU menory
                         dict(
-                            golden_svg_path = golden_svg_path,
-                            generated_svg_path = decoded_svg_path,
+                            golden_svg_path = golden_svg_path.cpu(),
+                            generated_svg_path = decoded_svg_path.cpu(),
                             text = text,
-                            svg_token_ids = svg_token_ids,
+                            svg_token_ids = svg_token_ids.cpu(),
                         )
                     )
                     
@@ -101,6 +106,7 @@ def post_process(res: List[Dict], save_dir=None, generate_big_map=True, add_back
         g_svg, g_svg_str = convert_svg(golden, True)
 
         str_paths.append({
+            "text": text,
             "p_svg_str": p_svg_str,
             "g_svg_str": g_svg_str,
         })
@@ -110,17 +116,26 @@ def post_process(res: List[Dict], save_dir=None, generate_big_map=True, add_back
     
     auto_save_data(str_paths, SVG_PATH_SAVED_PATH)
     
-    for i, item in enumerate(res):
-        golden_svg_path = item['golden_svg_path']
-        generated_svg_path = item['generated_svg_path']
-        text_input_ids = item['text_input_ids']
+    if generate_big_map:
+        print_c("begin to generate big map", "magenta")
+        BIG_MAP_SAVED_DIR = auto_mkdir(os.path.join(save_dir, "rendered_big_map"))
+        p_svg_images = merge_images(
+            folder_path=SINGLE_IMAGE_SAVED_DIR, 
+            image_suffix='p_svg.png', 
+            num_images=500, 
+            save_dir=BIG_MAP_SAVED_DIR
+        )
         
-        golden_svg_path.save_png(f"golden_{i}.png")
-        generated_svg_path.save_png(f"generated_{i}.png")
-        print(f"text input ids: {text_input_ids}")
-        print(f"golden svg path: {golden_svg_path}")
-        print(f"generated svg path: {generated_svg_path}")
-        print("=============================================")
+    if add_background:
+        print_c(f"add background to {len(all_image_paths)} images", "magenta")
+        for i in trange(len(all_image_paths)):
+            image_path = all_image_paths[i]
+            if "_b.png" in image_path:
+                continue
+            add_background(image_path=image_path)
+            
+    if save_intermediate_results:
+        raise NotImplementedError("save_intermediate_results is not implemented yet!")
         
 
 def test():
@@ -130,13 +145,9 @@ def test():
     # parsing vqvae_config:
     vqvae_config = load_yaml_config(test_args.vqvae_config_path)
 
-    # config 
-    llamaconfig = transformers.LlamaConfig.from_pretrained(test_args.model_name_or_path)
-    llamaconfig.frozen_llm = False
-    llamaconfig.max_text_length = 64
-    llamaconfig.svg_token_dims = 4096
-    llamaconfig.min_path_nums = 4
-    llamaconfig.max_path_nums = 512
+    # parsing trained model path
+    SAVE_DIR="/zecheng2/vqllama/vqllama_llama"
+    MODEL_NAME_OR_PATH = os.path.join(SAVE_DIR, f"version_{test_args.version}/checkpoint-{test_args.epoch}")
     
     llama_tokenizer = transformers.AutoTokenizer.from_pretrained(
         test_args.tokenier_config_path,
@@ -144,6 +155,14 @@ def test():
         padding_side="right",
         use_fast=True,
     )
+    
+    # model config 
+    llamaconfig = transformers.LlamaConfig.from_pretrained(MODEL_NAME_OR_PATH)
+    llamaconfig.frozen_llm = False
+    llamaconfig.max_text_length = 64
+    llamaconfig.svg_token_dims = 4096
+    llamaconfig.min_path_nums = 4
+    llamaconfig.max_path_nums = 512
     
     svg_data_module = VQLLaMAData(
         llamaconfig, 
@@ -157,22 +176,21 @@ def test():
     predict_dataloader = svg_data_module.predict_dataloader()
 
     svgllama = VQSVGLlama.from_pretrained(
-        test_args.model_name_or_path, 
+       MODEL_NAME_OR_PATH, 
         config=llamaconfig, 
         codebook_size=vqvae_config.vqvae.l_bins,
     )
-
-    if "llama" in test_args.model_name_or_path.lower():
-        # add new tokens and resize embedding & LM head
-        added_tokens = {
-            "eos_token": DEFAULT_EOS_TOKEN,
-            "bos_token": DEFAULT_BOS_TOKEN,
-            "pad_token": DEFAULT_PAD_TOKEN,
-            "additional_special_tokens": [DEFAULT_SVG_BEGIN_TOKEN],
-        }
-        smart_tokenizer_and_embedding_resize(
-            added_tokens, llama_tokenizer, svgllama
-        )
+    
+    # add new tokens and resize embedding & LM head
+    added_tokens = {
+        "eos_token": DEFAULT_EOS_TOKEN,
+        "bos_token": DEFAULT_BOS_TOKEN,
+        "pad_token": DEFAULT_PAD_TOKEN,
+        "additional_special_tokens": [DEFAULT_SVG_BEGIN_TOKEN],
+    }
+    smart_tokenizer_and_embedding_resize(
+        added_tokens, llama_tokenizer, svgllama
+    )
 
     svg_begin_token_id = llama_tokenizer.convert_tokens_to_ids(DEFAULT_SVG_BEGIN_TOKEN)
     svgllama.add_svg_begin_token_id(svg_begin_token_id)
@@ -187,7 +205,6 @@ def test():
         dilation_cycle=vqvae_config.vqvae_conv_block.dilation_cycle,
         reverse_decoder_dilation=vqvae_config.vqvae_conv_block.vqvae_reverse_decoder_dilation
     )
-    # offline inference version
     vqvae = VQVAE(vqvae_config, multipliers=None, **block_kwargs)
     plugin_vqvae = PluginVQVAE(vqvae)
     checkpoint = torch.load(vqvae_config.ckpt_path)  # load vqvae ckpt
@@ -195,11 +212,12 @@ def test():
     print_c("VQVAE loaded!", "green")
     vqvae = plugin_vqvae.model
     
-    svgllama.eval()
-    vqvae.eval()
+    svgllama.eval().cuda()
+    vqvae.eval().cuda()
     
     if test_args.fp16:
         svgllama = svgllama.half()
+        vqvae = vqvae.half()
         
     sampling_strategy = dict(
         do_sample=test_args.do_sample,
@@ -216,6 +234,14 @@ def test():
         tokenizer=llama_tokenizer,
         max_generate_length=test_args.max_generate_length,
         **sampling_strategy,
+    )
+    
+    post_process(
+        predicted_results, 
+        save_dir=test_args.save_image_dir, 
+        generate_big_map=True, 
+        add_background=False, 
+        save_intermediate_results=False
     )
     
    
