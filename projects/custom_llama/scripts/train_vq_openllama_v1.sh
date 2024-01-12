38668254c26ce15c0238cb3aecad39ae50313ae8
ZetangForward <zecheng.tang@foxmail.com>
2024-01-12 22:43:23 +0800

    dsa

--- projects/custom_llama/scripts/train_vq_openllama_v1.sh
+++ projects/custom_llama/scripts/train_vq_openllama_v1.sh
@@ -14,7 +14,7 @@ deepspeed --num_gpus 16 \
     --output_dir ${OUTPUT_DIR} \
     --num_train_epochs 20 \
     --model_max_length 1024 \
-    --per_device_train_batch_size 78 \
+    --per_device_train_batch_size 72 \
     --per_device_eval_batch_size 16 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
