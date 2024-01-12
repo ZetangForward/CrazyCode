dda6dd96551700a284920f14d398b361d1f3bfc4
ZetangForward <zecheng.tang@foxmail.com>
2024-01-12 19:17:18 +0800

    dsa

--- projects/custom_llama/baselines/icon-shop/iconshop-train.sh
+++ projects/custom_llama/baselines/icon-shop/iconshop-train.sh
@@ -9,15 +9,15 @@ deepspeed --num_gpus 8 \
     --output_dir "/zecheng2/vqllama/baselines/iconshop" \
     --num_train_epochs 40 \
     --model_max_length 1024 \
-    --per_device_train_batch_size 18 \
-    --per_device_eval_batch_size 18 \
+    --per_device_train_batch_size 24 \
+    --per_device_eval_batch_size 24 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
-    --eval_steps 100 \
+    --eval_steps 80 \
     --greater_is_better False \
     --save_strategy "steps" \
     --load_best_model_at_end True \
-    --save_steps 100 \
+    --save_steps 80 \
     --metric_for_best_model "loss" \
     --save_total_limit 10 \
     --learning_rate 3e-6 \
