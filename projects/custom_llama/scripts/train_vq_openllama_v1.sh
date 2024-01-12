d337842b0a685cdba23c339bb7c1b1d4ed8558f3
ZetangForward <zecheng.tang@foxmail.com>
2024-01-12 22:07:56 +0800

    dsa

--- projects/custom_llama/scripts/train_vq_openllama_v1.sh
+++ projects/custom_llama/scripts/train_vq_openllama_v1.sh
@@ -5,7 +5,7 @@ mkdir -p ${OUTPUT_DIR}
 deepspeed --num_gpus 16 \
     --num_nodes 4 \
     --master_addr worker-0 \
-    --master_port 6529 \
+    --master_port 6429 \
     --hostfile configs/machine/hostfile_v64_sxm4 \
     train_vqllama.py \
     --model_name_or_path "/zecheng2/vqllama/vqllama_openllama/version_1/checkpoint-700" \
