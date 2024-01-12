7ffb6c22b3180306eb2b46df5875d2cea8ddbb4b
ZetangForward <zecheng.tang@foxmail.com>
2024-01-12 19:25:50 +0800

    dsa

--- projects/custom_llama/baselines/icon-shop/iconshop-train.sh
+++ projects/custom_llama/baselines/icon-shop/iconshop-train.sh
@@ -1,7 +1,7 @@
 deepspeed --num_gpus 8 \
     --num_nodes 3 \
     --master_addr worker-0 \
-    --master_port 6229 \
+    --master_port 6129 \
     --hostfile "/workspace/zecheng/modelzipper/projects/custom_llama/configs/machine/hostfile_v24" \
     icon-shop/train.py \
     --model_name_or_path "/zecheng2/model_hub/open_llama_3b_v2" \
@@ -9,8 +9,8 @@ deepspeed --num_gpus 8 \
     --output_dir "/zecheng2/vqllama/baselines/iconshop" \
     --num_train_epochs 60 \
     --model_max_length 1024 \
-    --per_device_train_batch_size 30 \
-    --per_device_eval_batch_size 30 \
+    --per_device_train_batch_size 32 \
+    --per_device_eval_batch_size 32 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "epoch" \
     --greater_is_better False \
