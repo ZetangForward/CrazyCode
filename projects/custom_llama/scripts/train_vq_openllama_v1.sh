58f295b645fd7a44a230e3fc8742e51ed35fcd1c
ZetangForward <zecheng.tang@foxmail.com>
2024-01-12 13:05:43 +0800

    dsa

--- projects/custom_llama/scripts/train_vq_openllama_v1.sh
+++ projects/custom_llama/scripts/train_vq_openllama_v1.sh
@@ -1,25 +1,27 @@
-VERSION=0
-OUTPUT_DIR="/zecheng2/vqllama/vqllama_openllama/version_${VERSION}"
+OUTPUT_DIR="/zecheng2/vqllama/vqllama_openllama/version_1"
 
 mkdir -p ${OUTPUT_DIR}
 
 deepspeed --num_gpus 8 \
-    --num_nodes 1 \
+    --num_nodes 3 \
+    --master_addr worker-0 \
+    --master_port 6429 \
+    --hostfile configs/machine/hostfile_v24 \
     train_vqllama.py \
     --model_name_or_path "/zecheng2/model_hub/open_llama_3b_v2" \
-    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_7.pkl" \
+    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_merged.pkl" \
     --output_dir ${OUTPUT_DIR} \
     --num_train_epochs 100 \
-    --model_max_length 1024 \
+    --model_max_length 1500 \
     --per_device_train_batch_size 72 \
     --per_device_eval_batch_size 16 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "steps" \
-    --eval_steps 80 \
+    --eval_steps 100 \
     --greater_is_better False \
     --save_strategy "steps" \
     --load_best_model_at_end True \
-    --save_steps 80 \
+    --save_steps 100 \
     --save_total_limit 10 \
     --learning_rate 3e-5 \
     --warmup_steps 20 \
