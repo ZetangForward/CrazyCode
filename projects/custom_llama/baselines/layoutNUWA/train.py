6b8d2fd4e3a675b586c769240f04192e148282ec
ZetangForward <zecheng.tang@foxmail.com>
2024-01-13 11:51:53 +0800

    das

--- projects/custom_llama/baselines/layoutNUWA/train.py
+++ projects/custom_llama/baselines/layoutNUWA/train.py
@@ -147,15 +147,8 @@ class CustomTrainier(Trainer):
         )
         
     def compute_loss(self, model, inputs, return_outputs=False):
-        input_ids = inputs.get("input_ids")
-        attention_mask = inputs.get("attention_mask")
-        labels = inputs.get("labels")
         
-        outputs = model(
-            input_ids=input_ids,
-            attention_mask=attention_mask,
-            labels=labels,
-        )
+        outputs = model(**inputs)
         
         loss = outputs.loss
         return (loss, outputs) if return_outputs else loss 
