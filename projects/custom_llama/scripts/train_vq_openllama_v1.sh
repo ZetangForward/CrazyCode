f85db633bf1e0d4c68e0c882f02360688ff99652
ZetangForward <zecheng.tang@foxmail.com>
2024-01-14 01:04:04 +0800

    dsa

--- projects/custom_llama/scripts/train_vq_openllama_v1.sh
+++ projects/custom_llama/scripts/train_vq_openllama_v1.sh
@@ -4,14 +4,14 @@ OUTPUT_DIR="/zecheng2/vqllama/vqllama_openllama/version_1"
 
 mkdir -p ${OUTPUT_DIR}
 
-deepspeed --num_gpus 16 \
-    --num_nodes 4 \
+deepspeed --num_gpus 4 \
+    --num_nodes 16 \
     --master_addr worker-0 \
-    --master_port 6429 \
-    --hostfile configs/machine/hostfile_v64_sxm4 \
+    --master_port 6430 \
+    --hostfile configs/machine/hostfile_v64 \
     train_vqllama.py \
-    --model_name_or_path "/zecheng2/vqllama/vqllama_openllama/version_1/checkpoint-700" \
-    --resume_from_checkpoint "/zecheng2/vqllama/vqllama_openllama/version_1/checkpoint-700" \
+    --model_name_or_path "/zecheng2/vqllama/vqllama_openllama/version_1/checkpoint-3500" \
+    --resume_from_checkpoint "/zecheng2/vqllama/vqllama_openllama/version_1/checkpoint-3500" \
     --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer/version_8/epoch_84/inference_full_data_compress_1_snaps_merged.pkl" \
     --output_dir ${OUTPUT_DIR} \
     --num_train_epochs 20 \
@@ -24,8 +24,8 @@ deepspeed --num_gpus 16 \
     --save_strategy "steps" \
     --load_best_model_at_end True \
     --metric_for_best_model "loss" \
-    --eval_steps 100 \
-    --save_steps 100 \
+    --eval_steps 200 \
+    --save_steps 200 \
     --save_total_limit 10 \
     --learning_rate 3e-5 \
     --warmup_steps 20 \
