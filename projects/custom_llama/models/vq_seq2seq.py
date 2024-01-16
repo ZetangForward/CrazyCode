074690e7e59e65385f6d7eb27fb3d9e3ef2e841d
ZetangForward <zecheng.tang@foxmail.com>
2024-01-16 19:45:57 +0800

    dsa

--- projects/custom_llama/models/vq_seq2seq.py
+++ projects/custom_llama/models/vq_seq2seq.py
@@ -10,6 +10,8 @@ from transformers import T5Model
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from transformers.generation import GenerationMixin
 from modelzipper.tutils import *
+from transformers.modeling_outputs import Seq2SeqLMOutput
+
 
 class VQSVGSeq2SeqModel(T5Model):  
     def __init__(self, config, tokenizer=None, vqvae=None, codebook_size=8192):  
@@ -30,6 +32,7 @@ class VQSVGSeq2SeqModel(T5Model):
             print_c("Attention! encoder is freezed!")
             self.encoder.requires_grad_ = False # only freeze the encoder
             self.shared.requires_grad_ = False  # freeze the text embedding 
+
     
     def init_vqvae(self, vqvae):
         self.vqvae = vqvae
@@ -37,10 +40,12 @@ class VQSVGSeq2SeqModel(T5Model):
         for param in self.vqvae.model.parameters():
             param.requires_grad = False
 
+
     def load_state_dict(self, state_dict: Mapping[str, Any], strict: bool = True):
         return super().load_state_dict(state_dict, strict)
         
-    def forward(self, text_input_ids=None, text_attention_mask=None, text_labels=None, svg_tensors=None, svg_padding_mask=None, **kwargs): 
+        
+    def forward(self, text_input_ids=None, text_attention_mask=None, text_labels=None, svg_tensors=None, svg_padding_mask=None, return_dict=None, **kwargs): 
         """
             text_input_ids: B x L 
             text_attention_mask: B x L,
@@ -127,93 +132,22 @@ class VQSVGSeq2SeqModel(T5Model):
             shift_golden_svg_tokens = shift_golden_svg_tokens.view(-1)
             loss = F.cross_entropy(shift_svg_logits, shift_golden_svg_tokens)
 
-        metrics = {}
+        if not return_dict:
+            output = (svg_logits,) + decoder_outputs[1:] + encoder_outputs
+            return ((loss,) + output) if loss is not None else output
         
-        if not self.training:
-            metrics['eval_loss'] = loss
-        else:
-            metrics['train_loss'] = loss
-        
-        return metrics
-    
-    
-    @torch.no_grad()
-    def generate(self, text_input_ids=None, text_attention_mask=None, past_key_values=None, max_generate_length=1024, do_sample=False, top_p=0.9, top_k=40, temperature=0.7, num_beams=1) -> List[torch.LongTensor]:
-       
-        if self.svg_begin_token_id in text_input_ids:
-            svg_being_token_pos = text_input_ids == self.svg_begin_token_id
-            text_input_ids[svg_being_token_pos] = self.tokenizer.pad_token_id 
-            text_attention_mask[svg_being_token_pos] = 0
-            
-        assert self.svg_begin_token_id not in text_input_ids, "You should not add svg_begin_token_id in text_input_ids, since it will automactically add svg_begin_token_id in the beginning of svg_tensors during the inference!"
-        
-        batch_size = text_input_ids.size(0)
-  
-        # initial eos_generated_mask to False for all samples as no sample has generated eos_token yet
-        eos_generated_mask = torch.zeros((batch_size, 1), dtype=torch.bool).to(text_input_ids.device)
-        
-        outputs = self.model(
-            input_ids=text_input_ids,
-            past_key_values=past_key_values,
-            attention_mask=text_attention_mask,
-            use_cache=True,
+        return Seq2SeqLMOutput(
+                loss=loss,
+                logits=svg_logits,
+                past_key_values=decoder_outputs.past_key_values,
+                decoder_hidden_states=decoder_outputs.hidden_states,
+                decoder_attentions=decoder_outputs.attentions,
+                cross_attentions=decoder_outputs.cross_attentions,
+                encoder_last_hidden_state=encoder_outputs.last_hidden_state,
+                encoder_hidden_states=encoder_outputs.hidden_states,
+                encoder_attentions=encoder_outputs.attentions,
         )
-        last_hidden_state = outputs.last_hidden_state
-        past_key_values = outputs.past_key_values
-        text_width = text_input_ids.size(1)
-        
-        generated_ids = [text_input_ids[:, i].unsqueeze(1) for i in range(text_width)]
-        
-        # create svg_begin token id and embeddings
-        svg_begin_token_ids = torch.empty(text_input_ids.size(0)).fill_(self.svg_begin_token_id).long().to(last_hidden_state.device)
-        
-        prev_svg_token_ids = svg_begin_token_ids.unsqueeze(1)
-        
-        text_embedding_module = self.base_model.get_input_embeddings()
-        input_embeddings = text_embedding_module(prev_svg_token_ids)
 
-        for _ in range(max_generate_length - 1):
-            outputs = self.model(
-                input_ids=None,
-                past_key_values=past_key_values,
-                inputs_embeds=input_embeddings, 
-                use_cache=True,
-            )
-            last_hidden_state = outputs.last_hidden_state
-            past_key_values = outputs.past_key_values
-            pred_h = self.down_adapter(last_hidden_state)
-            pred_logits = self.vqvae_head(pred_h).float()
-            
-            if do_sample:
-                pred_svg_idx = top_k_top_p_sampling(pred_logits[:, -1], top_k=top_k, top_p=top_p, temperature=temperature, num_samples=num_beams).view(batch_size, -1)
-            else:
-                pred_svg_idx = pred_logits[:, -1].argmax(dim=-1).unsqueeze(1)
-            
-            # update eos_generated_mask, as some samples generate svg_eos_token
-            eos_generated_mask |= (pred_svg_idx == self.svg_end_token_id)  
-
-            # add the predicted svg token embedding to input_embeddings according to pred_svg_idx
-            current_step_ids = torch.full((batch_size, 1), self.svg_end_token_id, dtype=torch.long, device=last_hidden_state.device)  
-           
-            current_step_ids[~eos_generated_mask] = pred_svg_idx[~eos_generated_mask]  
-            generated_ids.append(current_step_ids)  
-            
-            if eos_generated_mask.all():  # all samples have generated eos_token
-                break
-            
-            prev_svg_token_ids = current_step_ids
-            input_embeddings = self.vqvae_embedding(prev_svg_token_ids)
-            input_embeddings = self.up_adapter(input_embeddings)
-            
-        generated_ids = torch.cat(generated_ids, dim=1)  # B x gen_length
-        generated_mask = ~(generated_ids == self.svg_end_token_id)  # B x gen_length
-        post_processed_ids = []  # List[Tensor]
-        
-        for i in range(batch_size):
-            post_processed_ids.append(generated_ids[i, text_width: generated_mask[i].sum()])
-
-        return generated_ids, post_processed_ids
-        
     
     @property
     def model_device(self):
