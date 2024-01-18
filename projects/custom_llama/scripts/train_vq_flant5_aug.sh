92e0a06115b77ca40bd4cc7879e27854c1ddd5d0
ZetangForward <zecheng.tang@foxmail.com>
2024-01-18 12:45:48 +0800

    das

--- projects/custom_llama/scripts/train_vq_flant5_aug.sh
+++ projects/custom_llama/scripts/train_vq_flant5_aug.sh
@@ -10,20 +10,16 @@ deepspeed --num_gpus 8 \
     train_vq_seq2seq.py \
     --model_name_or_path "/zecheng2/vqllama/vqllama_flant5/version_1/checkpoint-3000" \
     --resume_from_checkpoint "/zecheng2/vqllama/vqllama_flant5/version_1/checkpoint-3000" \
-    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer_testset/version_12/epoch_37/inference_full_data_compress_1_snaps_merged.pkl" \
+    --data_path "/zecheng2/svg/icon-shop/pkl_data/efficient_inference_full_data/test_vqllama_quantizer_testset/version_12/epoch_37/augment_stage2_data.pkl" \
     --output_dir ${OUTPUT_DIR} \
     --num_train_epochs 20 \
     --model_max_length 512 \
     --per_device_train_batch_size 54 \
     --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 1 \
-    --evaluation_strategy "steps" \
-    --greater_is_better False \
+    --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --load_best_model_at_end True \
-    --metric_for_best_model "loss" \
-    --eval_steps 300 \
-    --save_steps 300 \
+    --save_steps 200 \
     --save_total_limit 10 \
     --learning_rate 5e-5 \
     --warmup_steps 20 \
@@ -31,7 +27,7 @@ deepspeed --num_gpus 8 \
     --dataloader_num_workers 0 \
     --report_to "tensorboard" \
     --gradient_checkpointing True \
-    --deepspeed configs/deepspeed/stage3_test.json \
+    --deepspeed configs/deepspeed/stage3.json \
     --fp16 False \
     --remove_unused_columns False \
     --freezen_llm True \
