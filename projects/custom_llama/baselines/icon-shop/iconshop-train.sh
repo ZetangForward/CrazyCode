83454cd4aaff73a3471003376ac4ed42d13d1307
ZetangForward <zecheng.tang@foxmail.com>
2024-01-19 00:29:27 +0800

    dsa

--- projects/custom_llama/baselines/icon-shop/iconshop-train.sh
+++ projects/custom_llama/baselines/icon-shop/iconshop-train.sh
@@ -27,7 +27,8 @@ deepspeed --num_gpus 8 \
     --gradient_checkpointing True \
     --deepspeed "/workspace/zecheng/modelzipper/projects/custom_llama/configs/deepspeed/stage3.json" \
     --fp16 False \
-    --remove_unused_columns False;
+    --remove_unused_columns False \
+    --freezen_llm True \
 
 
 
