4bf683745e78a31fec3e095055510e3550bbdf4b
ZetangForward <zecheng.tang@foxmail.com>
2024-01-21 04:01:18 +0800

    dsa

--- projects/custom_llama/scripts/train_vq_flant5_aug.sh
+++ projects/custom_llama/scripts/train_vq_flant5_aug.sh
@@ -5,7 +5,7 @@ mkdir -p ${OUTPUT_DIR}
 deepspeed --num_gpus 16 \
     --num_nodes 4 \
     --master_addr worker-0 \
-    --master_port 5668 \
+    --master_port 6668 \
     --hostfile configs/machine/hostfile_v64_sxm4 \
     train_vq_seq2seq_aug.py \
     --model_name_or_path "/zecheng2/vqllama/vqllama_flant5/version_aug_v6/checkpoint-484" \
