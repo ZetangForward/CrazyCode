43ecbca57b61254c78de4634554c8283b24f4174
ZetangForward <zecheng.tang@foxmail.com>
2024-02-15 11:44:43 +0800

    dsa

--- projects/ModelAlign/detoxification/run.sh
+++ projects/ModelAlign/detoxification/run.sh
@@ -2,7 +2,7 @@ CONFIG=configs/accelerate_configs/single_gpu.yaml
 MODEL_SAVE_PATH=/home/amax/zecheng/ckpts/gpt-neo-1.3B
 
 export CUDA_VISIBLE_DEVICES=2,3,4,5
-ACCELERATE_LOG_LEVEL=info accelerate launch --config_file ${CONFIG} detoxification/example.py \
+WANDB_MODE=offline ACCELERATE_LOG_LEVEL=info accelerate launch --config_file ${CONFIG} detoxification/example.py \
     --log_with wandb \
     --batch_size 32 \
     --model_save_path ${MODEL_SAVE_PATH} \
