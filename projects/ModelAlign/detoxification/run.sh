37291593927c9349507b9fce73d8a0f1baa5d0c8
ZetangForward <zecheng.tang@foxmail.com>
2024-02-15 11:36:27 +0800

    dsa

--- projects/ModelAlign/detoxification/run.sh
+++ projects/ModelAlign/detoxification/run.sh
@@ -3,7 +3,8 @@ MODEL_SAVE_PATH=/home/amax/zecheng/ckpts/gpt-neo-1.3B
 
 export CUDA_VISIBLE_DEVICES=2,3,4,5
 ACCELERATE_LOG_LEVEL=info accelerate launch --config_file ${CONFIG} detoxification/example.py \
-    --log_with tensorboard \
+    --log_with wandb \
+    --batch_size 32 \
     --model_save_path ${MODEL_SAVE_PATH} \
     --model_name "/nvme/hf_models/gpt-neo-1.3B" \
     --dataset_name "/home/amax/zecheng/data/real-toxicity-prompts" \
