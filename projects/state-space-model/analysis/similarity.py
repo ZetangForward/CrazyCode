0d182aa9f361efc602d60d3694ba23547e784475
ZetangForward <zecheng.tang@foxmail.com>
2024-03-07 15:42:02 +0800

    dsa

--- projects/state-space-model/analysis/similarity.py
+++ projects/state-space-model/analysis/similarity.py
@@ -19,6 +19,7 @@ def analysis_cov1d_compress(fpath):
 
     fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(200, 50)) 
     axs = axs.flatten() 
+
     for idx, file_name in enumerate(file_names):
         layer_idx = int(os.path.basename(file_name).split("-")[-1].split(".")[0])
         hidden_state = auto_read_data(file_name) 
@@ -35,24 +36,44 @@ def analysis_cov1d_compress(fpath):
         for i, h in enumerate(h_avg_pooled):
             cos_sim = F.cosine_similarity(h.unsqueeze(0), text_embedding)
             similarity_matrix[i] = cos_sim
-
+        
         similarity_matrix_np = similarity_matrix.cpu().numpy()
         top_indices = np.argpartition(similarity_matrix_np, -50, axis=1)[:, -50:]
+        
+        # 统计每个区间出发的关键点
+         
+        num_partitions = 5
+        partition_length = similarity_matrix.size(-1) // num_partitions  # 每个分区的长度
+        partition_scores = np.zeros((top_indices.shape[0], num_partitions))  # 存储每个分区的得分
+
+        # 对 top_indices 进行排序
+        top_indices_sorted = np.sort(top_indices, axis=1)
+
+        # 计算每个分区的得分
+        for j in range(num_partitions):
+            start_idx = j * partition_length
+            end_idx = start_idx + partition_length
+            # 计算每个分区中top_indices的数量
+            for k in range(top_indices_sorted.shape[0]):  # 遍历每个压缩的状态
+                count_in_partition = np.sum((top_indices_sorted[k, :] >= start_idx) & (top_indices_sorted[k, :] < end_idx))
+                partition_scores[k, j] = count_in_partition / 50.0  # 计算比例
+       
         mask = np.zeros_like(similarity_matrix_np, dtype=bool)
         for i in range(similarity_matrix_np.shape[0]):
             mask[i, top_indices[i]] = True
 
-        # 使用当前子图绘制
         ax = axs[idx]
         ax.imshow(similarity_matrix_np, cmap='Reds', aspect='auto', alpha=0.3) 
         ax.imshow(mask, cmap='hot', aspect='auto', alpha=0.9)
+        
+        print_c(f"Layer {layer_idx} partition_scores: {partition_scores}", "yellow")
+        
         ax.axis('off')
         ax.set_title(f"Layer {layer_idx}")
 
     # 调整子图位置
     plt.tight_layout()
     plt.savefig("analysis/figures/cosine_similarity_heatmap_combined.png")
-    plt.show()
 
 if __name__ == "__main__":
     argparse = ArgumentParser()
