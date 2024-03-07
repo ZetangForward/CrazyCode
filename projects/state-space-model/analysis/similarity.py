a53984a541b36fa1478f699b435e3fa6f3ac878f
ZetangForward <zecheng.tang@foxmail.com>
2024-03-07 20:17:56 +0800

    dsa

--- projects/state-space-model/analysis/similarity.py
+++ projects/state-space-model/analysis/similarity.py
@@ -1,14 +1,13 @@
 from argparse import ArgumentParser
 import os
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 from modelzipper.tutils import *
 
 
 
-def analysis_cov1d_compress(fpath):
+def analysis_cov1d_compress(fpath, highlight_start=18, highlight_end=40):
     file_names = auto_read_dir(fpath, file_prefix="passkey", file_suffix=".pkl")
     file_names = sorted(file_names, key=lambda x: int(os.path.basename(x).split("-")[-1].split(".")[0]))
     
@@ -17,11 +16,16 @@ def analysis_cov1d_compress(fpath):
     if text_embedding.dim() == 3:
         text_embedding = text_embedding.squeeze(0)
 
-    fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(200, 50)) 
+    fig, axs = plt.subplots(nrows=5, ncols=1, figsize=(200, 50))
+    plt.subplots_adjust(hspace=0.5)  # 调整子图之间的垂直间距
+
     axs = axs.flatten() 
+    layers, per_layer_scores = [], {}
 
     for idx, file_name in enumerate(file_names):
         layer_idx = int(os.path.basename(file_name).split("-")[-1].split(".")[0])
+        layers.append(layer_idx)
+
         hidden_state = auto_read_data(file_name) 
         
         if hidden_state.dim() == 3:
@@ -41,7 +45,14 @@ def analysis_cov1d_compress(fpath):
         top_indices = np.argpartition(similarity_matrix_np, -50, axis=1)[:, -50:]
         
         # 统计每个区间出发的关键点
-         
+        ### 真实数据的位置
+        highlight_mask = np.zeros_like(similarity_matrix_np, dtype=bool)
+        highlight_mask[:, highlight_start: highlight_end] = True
+        # 创建一个新的掩码,将highlight_mask部分设置为红色
+        green_mask = np.zeros((*similarity_matrix_np.shape, 3))
+        green_mask[highlight_mask, 1] = 1.0  # 绿色通道设为1  
+
+        ### 划分每个区间
         num_partitions = 5
         partition_length = similarity_matrix.size(-1) // num_partitions  # 每个分区的长度
         partition_scores = np.zeros((top_indices.shape[0], num_partitions))  # 存储每个分区的得分
@@ -57,7 +68,9 @@ def analysis_cov1d_compress(fpath):
             for k in range(top_indices_sorted.shape[0]):  # 遍历每个压缩的状态
                 count_in_partition = np.sum((top_indices_sorted[k, :] >= start_idx) & (top_indices_sorted[k, :] < end_idx))
                 partition_scores[k, j] = count_in_partition / 50.0  # 计算比例
-       
+        
+        per_layer_scores[layer_idx] = partition_scores
+        
         mask = np.zeros_like(similarity_matrix_np, dtype=bool)
         for i in range(similarity_matrix_np.shape[0]):
             mask[i, top_indices[i]] = True
@@ -65,22 +78,51 @@ def analysis_cov1d_compress(fpath):
         ax = axs[idx]
         ax.imshow(similarity_matrix_np, cmap='Reds', aspect='auto', alpha=0.3) 
         ax.imshow(mask, cmap='hot', aspect='auto', alpha=0.9)
-        
-        print_c(f"Layer {layer_idx} partition_scores: {partition_scores}", "yellow")
-        
-        ax.axis('off')
-        ax.set_title(f"Layer {layer_idx}")
+        ax.imshow(green_mask, aspect='auto', alpha=0.3)  # 显示红色掩码
 
+        # 绘制竖线表示切分位置
+        for j in range(1, num_partitions):
+            ax.axvline(x=j * partition_length, color='yellow', linestyle='--', linewidth=20)
+
+        ax.set_title(f"Layer {layer_idx}", fontsize=24)
+        # print_c(f"Layer {layer_idx} partition_scores:\n{partition_scores * 100}", "yellow")
+        
     # 调整子图位置
+    # plt.tight_layout()
+    # plt.savefig("analysis/figures/cosine_similarity_heatmap_combined.png")
+
+    
+    # 绘制 per_layer_scores 折线图
+    fig, axs = plt.subplots(nrows=1, ncols=5, figsize=(25, 5))
+    axs = axs.flatten()
+
+    x_labels = ['20%', '40%', '60%', '80%', '100%']
+    x_positions = np.arange(len(x_labels))
+    
+    for j in len(per_layer_scores.keys()):
+        for k in range(per_layer_scores[j].shape[0]):  # 循环每一种压缩程度
+            axs[j].plot(x_positions, per_layer_scores[j][k, :], marker='o', label=f'Conv1d-State-{k}')
+        axs[j].set_xticks(x_positions)
+        axs[j].set_xticklabels(x_labels)
+        axs[j].set_xlabel('Percentage of Text')
+        axs[j].set_ylabel('Percentage of Top Indices')
+        axs[j].set_title(f'Layer {j+1}')
+        axs[j].legend()  # 添加图例
+
     plt.tight_layout()
-    plt.savefig("analysis/figures/cosine_similarity_heatmap_combined.png")
+    plt.savefig("analysis/figures/partition_scores_line_plot.png")
+
 
 if __name__ == "__main__":
     argparse = ArgumentParser()
     argparse.add_argument("--fpath", type=str, default="analysis/inner_state")
-
+    argparse.add_argument("--tokenizer_name_or_path", type=str, default="analysis/inner_state")
 
     args = argparse.parse_args()
 
 
-    analysis_cov1d_compress(args.fpath)
+    analysis_cov1d_compress(
+        args.fpath, 
+        highlight_start=18, 
+        highlight_end=40
+    )
