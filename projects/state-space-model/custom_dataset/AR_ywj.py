03f389ebb798b70270d681c82ee2f8fdf1eb51c4
Moriarty0923 <1316832321@qq.com>
2024-03-09 21:18:07 +0800

    test

--- projects/state-space-model/custom_dataset/AR_ywj.py
+++ projects/state-space-model/custom_dataset/AR_ywj.py
@@ -1,1 +1,17 @@
-print(11)
+from modelzipper.datamanager import *
+from modelzipper.tutils import *
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader, Dataset
+import pytorch_lightning as pl
+import torch
+import glob
+
+class MQAR(Dataset):
+    def __init__(self, content=None, tokenizer=None, split="train", *args, **kwargs):
+        super().__init__()
+        self.split = split
+        self.content = content
+        self.tokenizer = tokenizer
+        self.max_seq_length = kwargs["max_seq_length"]
+        self.cluster_batch = kwargs["cluster_batch"]
+        self.filter_length(kwargs["testing_max_ctx"])
