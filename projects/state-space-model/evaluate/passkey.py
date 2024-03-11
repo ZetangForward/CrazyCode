3c07f800738549df51cdfeaa598b95c786173cb1
ZetangForward <zecheng.tang@foxmail.com>
2024-03-11 11:02:12 +0800

    dsa

--- projects/state-space-model/evaluate/passkey.py
+++ projects/state-space-model/evaluate/passkey.py
@@ -1,1 +1,16 @@
 from modelzipper.tutils import *
+from rouge_score import rouge_scorer
+import tensor_parallel as tp
+
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
+
+
+class Evaluator:
+
+    def __init__(self, fpath, task) -> None:
+        self.task = task
+        self.prediction = auto_read_data(fpath)
+        
+
+def eval_passkey_search():
+    pass
