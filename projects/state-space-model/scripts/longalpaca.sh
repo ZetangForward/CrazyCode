8f31c645f62ac831f7fc821b67ee4d0ef459bac6
Moriarty0923 <1316832321@qq.com>
2024-03-14 22:06:14 +0800

    dsa

---
+++ projects/state-space-model/scripts/longalpaca.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+model_name=mamba-1_4b
+num_devices=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
+platform=$1
+task=longalpaca
+
+nproc_per_node=$num_devices
+device_num=$num_devices
+
+echo "Number of devices: $num_devices"
+
+echo "Available GPU device IDs: $CUDA_VISIBLE_DEVICES"
+
+torchrun --nnode=1 --nproc_per_node=$nproc_per_node --master_port 6789  mamba/train.py \
+    experiment.hf_trainer=False \
+    JOB_ID=longalpaca_1 \
+    model=$model_name \
+    model_name=$model_name \
+    task=$task \
+    platform=$platform \
+    experiment.debug=False \
+    experiment.device_num=$device_num \
+    task.dataset.cluster_batch=False \
+    task.dataset.train_batch_size=1 \
+    task.dataset.max_seq_length=8192;
+
