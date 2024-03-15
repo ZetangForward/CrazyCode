1f9287a4646f0a7801f7b4c9bfc8a6cdeeecb8ad
ZetangForward <zecheng.tang@foxmail.com>
2024-03-15 23:56:17 +0800

    dsa

--- projects/state-space-model/scripts/mqar.sh
+++ projects/state-space-model/scripts/mqar.sh
@@ -1,5 +1,5 @@
-export CUDA_VISIBLE_DEVICES=4,5,6,7
-mkdir -p /public/home/ljt/tzc/evaluation/AR_ywj/mamba-1_4b
+export CUDA_VISIBLE_DEVICES=7
+# mkdir -p /public/home/ljt/tzc/evaluation/AR_ywj/mamba-1_4b
 MODEL_N=2048
 MODEL_D=128
 data_n_list="1024 2048 4096"
@@ -12,9 +12,9 @@ do
 
         # JOB_ID=N4096_D256-N4096_D128
         python mamba/test.py \
-            model.ckpt_path=/public/home/ljt/tzc/ckpt/AR_ywj-mamba-1_4b/version_MQAR_C8192_N${MODEL_N}_D${MODEL_D}/AR_ywj/MQAR_C8192_N${MODEL_N}_D${MODEL_D}/checkpoints/last.ckpt \
+            model.ckpt_path=/nvme/zecheng/ckpt/AR_ywj-mamba-1_4b/version_MQAR_C8192_N${MODEL_N}_D${MODEL_D}/AR_ywj/MQAR_C8192_N${MODEL_N}_D${MODEL_D}/checkpoints/last.ckpt \
             model.load_model_state_dict=True \
-            platform=langchao \
+            platform='amax_a100' \
             task='AR_ywj' \
             exp_task='AR_ywj' \
             job_id=$JOB_ID \
@@ -26,7 +26,7 @@ do
 
         python evaluate/evaluator.py \
             --task AR_ywj \
-            --fpath /public/home/ljt/tzc/evaluation/AR_ywj/mamba-1_4b/version_$JOB_ID/results/predictions.pkl \
-            --save_evaluation_path /public/home/ljt/tzc/evaluation/AR_ywj/mamba-1_4b/version_$JOB_ID/results/eval.txt;
+            --fpath /nvme/zecheng/evaluation/AR_ywj/mamba-1_4b/version_$JOB_ID/results/predictions.pkl \
+            --save_evaluation_path /nvme/zecheng/evaluation/AR_ywj/mamba-1_4b/version_$JOB_ID/results/eval.txt;
     done
 done
