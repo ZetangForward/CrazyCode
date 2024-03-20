7cad46e4191d4a0ea0b18310e9a4651826d18645
ZetangForward <zecheng.tang@foxmail.com>
2024-03-20 20:29:27 +0800

    dsa

--- projects/state-space-model/scripts/370M_mamba.sh
+++ projects/state-space-model/scripts/370M_mamba.sh
@@ -23,10 +23,10 @@ CUDA_LAUNCH_BLOCKING=1 torchrun --nnode=1 --nproc_per_node=$nproc_per_node --mas
     experiment.low_rank_train=False \
     experiment.device_num=$device_num \
     experiment.use_deepspeed=True \
-    experiment.accumulate_grad_batches=6 \
+    experiment.accumulate_grad_batches=5 \
     experiment.save_top_k=5 \
     task.dataset.cluster_batch=False \
-    task.dataset.train_batch_size=10 \
+    task.dataset.train_batch_size=12 \
     task.dataset.max_seq_length=2048 \
     task.dataset.nworkers=12 \
     
