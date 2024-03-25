560bd3bc08105b33092f7284d80098b84114ada2
ZetangForward <zecheng.tang@foxmail.com>
2024-03-25 14:26:35 +0800

    12

--- projects/state-space-model/scripts/longbench.sh
+++ projects/state-space-model/scripts/longbench.sh
@@ -1,21 +1,21 @@
 # export CUDA_VISIBLE_DEVICES=$1
 num_devices=$(echo $CUDA_VISIBLE_DEVICES | tr ',' '\n' | wc -l)
 job_id=0
-model=$1
-# model=deepseek-1_3b
+model=mam
+model=deepseek-1_3b
 
-# python mamba/test.py \
-#     job_id=${job_id} \
-#     model=$model \
-#     model_name=$model \
-#     model.ckpt_path=/aifs4su/ziliwang/txw/InternLM/zecheng/ckpt/longalpaca/mamba-1_4b/checkpoints/last.ckpt \
-#     model.load_model_state_dict=True \
-#     task='longbench_ywj' \
-#     exp_task='longbench_ywj' \
-#     platform=h_800 \
-#     experiment.device_num=${num_devices} \
-#     experiment.results_save_dir=longbench_ywj/$model/results;
-#     # >/public/home/ljt/tzc/modelzipper/projects/state-space-model/scripts/${model}_${job_id}.log 2>&1 &
+python mamba/test.py \
+    job_id=${job_id} \
+    model=$model \
+    model_name=$model \
+    model.ckpt_path=/aifs4su/ziliwang/txw/InternLM/zecheng/ckpt/longalpaca/mamba-1_4b/checkpoints/last.ckpt \
+    model.load_model_state_dict=True \
+    task='longbench_ywj' \
+    exp_task='longbench_ywj' \
+    platform=h_800 \
+    experiment.device_num=${num_devices} \
+    experiment.results_save_dir=longbench_ywj/$model/results;
+    # >/public/home/ljt/tzc/modelzipper/projects/state-space-model/scripts/${model}_${job_id}.log 2>&1 &
 
 # wait 
 
