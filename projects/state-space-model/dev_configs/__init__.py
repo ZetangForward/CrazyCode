8a6e71374cec1e43243110dce6a0ffeedb90c48c
ZetangForward <zecheng.tang@foxmail.com>
2024-03-30 17:11:15 +0800

    dsa

--- projects/state-space-model/dev_configs/__init__.py
+++ projects/state-space-model/dev_configs/__init__.py
@@ -1,3 +1,9 @@
+import os
+import sys
+current_file_path = os.path.abspath(__file__)
+current_file_dir = os.path.dirname(current_file_path)
+sys.path.append(current_file_dir)
+
 from model_config import ModelConfig
 from optimizer_config import OptimizerConfig
 from platform_config import PlatformConfig
@@ -15,6 +21,17 @@ def merge_configs(default, custom):
     return final_config
 
 
+def merge_dotdicts(d1, d2):
+    merged = d1.copy()  # 做一个d1的浅拷贝
+    for k, v in d2.items():
+        if k in merged and isinstance(merged[k], dict) and isinstance(v, dict):
+            # 如果两个DotDict都有相同的key，并且对应的value也是字典，递归合并
+            merged[k] = merge_dotdicts(DotDict(merged[k]), v)
+        else:
+            # 如果不存在冲突或者其中一个value不是字典，直接更新值
+            merged[k] = v
+    return DotDict(merged)
+
 class DotDict(dict):
     __getattr__ = dict.__getitem__
     __setattr__ = dict.__setitem__
@@ -29,18 +46,30 @@ class DotDict(dict):
 
 class WrapConfigs:
 
-    def __init__(self, model_name_or_path, opt_name, platform_name, data_name) -> None:
+    def __init__(
+            self, 
+            model_name_or_path, 
+            model_configs,
+            opt_name,
+            opt_configs, 
+            platform_name, 
+            data_name,
+            task_configs,
+        ) -> None:
         self.model_name_or_path = model_name_or_path
+        self.model_configs = model_configs
         self.opt_name = opt_name
+        self.opt_configs = opt_configs
         self.platform_name = platform_name
         self.data_name = data_name
+        self.task_configs = task_configs
         self.all_configs = self.set_all_configs()
 
-    def all_configs(self):
-        model_config = ModelConfig(self.model_name_or_path)
-        optimizer_config = OptimizerConfig(self.opt_name)
+    def set_all_configs(self):
+        model_config = ModelConfig(self.model_name_or_path, **self.model_configs)
+        optimizer_config = OptimizerConfig(self.opt_name, **self.opt_configs)
         platform_config = PlatformConfig(self.platform_name)
-        task_config = TaskConfig(self.data_name)
+        task_config = TaskConfig(self.data_name, **self.task_configs)
 
         default_config = {
             "model": model_config,
--- projects/state-space-model/dev_configs/train_config.py
+++ projects/state-space-model/dev_configs/train_config.py
@@ -7,66 +7,85 @@ import argparse
 from modelzipper.tutils import *
 from . import *
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process some hyperparameters.')
 
-parser = argparse.ArgumentParser(description='Process some hyperparameters.')
+    # Configs of Model Hyper-parameters
+    parser.add_argument('--model_name_or_path', type=str, default='mamba-370m-k8', 
+                        help='Model name or path')
+    parser.add_argument('--tokenizer_name_or_path', type=str, default=None, 
+                        help='Tokenizer path. If not set, will use the model_name_or_path')
+    parser.add_argument('--ckpt_path', type=str, default=None, 
+                        help='ckpt path for model after training')
+    parser.add_argument('--use_relative_position',action='store_true',
+                        help='whether to use relative position embeddings')
+    parser.add_argument('--use_abs_position',action='store_true',
+                        help='whether to use absolute position embeddings')
+    parser.add_argument('--max_position_embeddings', type=int, default=None,
+                        help='if use_abs_position, set the max_position_embeddings')
 
-# Configs of Model Hyper-parameters
-parser.add_argument('--model_name_or_path', type=str, default='mamba-370m-k8', 
-                    required=True, help='Model name or path')
-parser.add_argument('--tokenizer_name_or_path', type=str, default=None, 
-                    help='Tokenizer path. If not set, will use the model_name_or_path')
-parser.add_argument('--ckpt_path', type=str, default=None, 
-                    help='ckpt path for model after training')
-parser.add_argument('--use_relative_position',action='store_true',
-                    help='whether to use relative position embeddings')
-parser.add_argument('--use_abs_position',action='store_true',
-                    help='whether to use absolute position embeddings')
-parser.add_argument('--max_position_embeddings', type=int, default=None,
-                    help='if use_abs_position, set the max_position_embeddings')
+    # Configs of Optimizer Hyper-parameters
+    parser.add_argument('--opt_name', type=str, default='adawm', 
+                        help='optimizer name')
+    parser.add_argument('--train_step', type=int, default=20000,
+                        help='set training steps')
+    parser.add_argument('--warmup_step', type=int, default=2000,
+                        help='set warmup steps')
 
-# Configs of Optimizer Hyper-parameters
-parser.add_argument('--opt_name', type=str, default='adawm', 
-                    required=True, help='optimizer name')
-parser.add_argument('--train_step', type=int, default=20000,
-                    help='set training steps')
-parser.add_argument('--warmup_step', type=int, default=2000,
-                    help='set warmup steps')
+    # Configs of Lr_Scheduler Hyper-parameters
+    parser.add_argument('--scheduler_type', type=str, default='get_cosine_schedule_with_warmup', 
+                        help='optimizer name')
 
-# Configs of Lr_Scheduler Hyper-parameters
-parser.add_argument('--scheduler_type', type=str, default='get_cosine_schedule_with_warmup', 
-                    required=True, help='optimizer name')
+    # Configs of Platform Hyper-parameters
+    parser.add_argument('--platform_name', '-pn', type=str, default='amax_a100',
+                        required=True, help='define platform name')
 
-# Configs of Platform Hyper-parameters
-parser.add_argument('--platform_name', type=str, default='amax_a100',
-                    required=True, help='define platform name')
+    # Configs of Task Hyper-parameters
+    parser.add_argument('--data_name', type=str, default='simplepajama',
+                        help='define task name')
 
-# Configs of Task Hyper-parameters
-parser.add_argument('--data_name', type=str, default='simplepajama',
-                    required=True, help='define task name')
+    # Configs of Training Hyper-parameters
+    parser.add_argument('--experiment_name', '-en', type=str, default=None,
+                        required=True, help='mark for the experiment (connect with the experiment name)')
+    parser.add_argument('--version', '-v', type=int, default=1,
+                        help='version of the experiments, if not set, default is 1')
+    parser.add_argument('--state', type=str, default='train', choices=['train', 'eval'],
+                        help='define the state of the experiment')
+    parser.add_argument('--accumulate_grad_batches', '-agb', type=int, default=1,
+                        help='accumulate_grad_batches')
+    parser.add_argument('--use_deepspeed', action='store_true', 
+                        help='Enable to use DeepSpeed optimization.')
+    parser.add_argument('--debug', action='store_true', 
+                        help='Enable to activate debug mode.')
+    parser.add_argument('--hf_trainer', action='store_true', 
+                        help='Enable to use Hugging Face Trainer.')
+    parser.add_argument('--low_rank_train', action='store_true', 
+                        help='Enable to use low rank training approach.')
+    parser.add_argument('--device_num', type=int, default=1, 
+                        help='Set the number of devices to use.')
+    parser.add_argument('--node_num', type=int, default=1, 
+                        help='Set the number of nodes for distributed training.')
 
-# Configs of Training Hyper-parameters
-parser.add_argument('--experiment_name', '-en', type=str, default=None,
-                    required=True, help='mark for the experiment (connect with the experiment name)')
-parser.add_argument('--version', '-v', type=int, default=1,
-                    help='version of the experiments, if not set, default is 1')
-parser.add_argument('--state', type=str, default='train', choices=['train', 'eval'],
-                    help='define the state of the experiment')
-parser.add_argument('--accumulate_grad_batches', '-agb', type=int, default=1,
-                    help='accumulate_grad_batches')
-parser.add_argument('--use_deepspeed', action='store_true', 
-                    help='Enable to use DeepSpeed optimization.')
-parser.add_argument('--debug', action='store_true', 
-                    help='Enable to activate debug mode.')
-parser.add_argument('--hf_trainer', action='store_true', 
-                    help='Enable to use Hugging Face Trainer.')
-parser.add_argument('--low_rank_train', action='store_true', 
-                    help='Enable to use low rank training approach.')
-parser.add_argument('--device_num', type=int, default=1, 
-                    help='Set the number of devices to use.')
-parser.add_argument('--node_num', type=int, default=1, 
-                    help='Set the number of nodes for distributed training.')
+    args = parser.parse_args()
+
+    return args
 
 
-args = parser.parse_args()
+def get_final_configs(args):
+    basic_configs = WrapConfigs(args.model_name_or_path, args.opt_name, args.platform_name, args.data_name).all_configs
+    train_configs = DotDict({
+        "experiment_name": args.experiment_name,
+        "version": args.version,
+        "state": args.state,
+        "accumulate_grad_batches": args.accumulate_grad_batches,
+        "use_deepspeed": args.use_deepspeed,
+        "debug": args.debug,
+        "hf_trainer": args.hf_trainer,
+        "low_rank_train": args.low_rank_train,
+        "device_num": args.device_num,
+        "node_num": args.node_num,
+    })
+    final_configs = merge_dotdicts(basic_configs, train_configs) # merge all configs
+    return final_configs
 
 
